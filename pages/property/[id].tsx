7e4fe4c466e719ed77ed089d1f14c385a248095e
hany-7878 <htesfaye641@gmail.com>
2025-12-01 05:46:06 -0800

    updated

--- pages/property/[id].tsx
+++ pages/property/[id].tsx
@@ -1,7 +1,5 @@
 import { useRouter } from "next/router";
 import PropertyDetail from "@/components/property/PropertyDetail";
-import BookingSection from "@/components/property/BookingSection";
-import ReviewSection from "@/components/property/ReviewSection";
 import { PROPERTYLISTINGSAMPLE } from "@/constants";
 import { PropertyProps } from "@/interfaces";
 
@@ -11,27 +9,15 @@ const PropertyPage: React.FC = () => {
 
   if (!id) return null; // Wait until id is ready
 
-  const property: PropertyProps | undefined = PROPERTYLISTINGSAMPLE.find((p) => p.id.toString() === id);
- 
+  const property: PropertyProps | undefined = PROPERTYLISTINGSAMPLE.find(
+    (p) => p.id.toString() === id
+  );
 
   if (!property) return <p>Property not found</p>;
 
   return (
     <main className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
-      {/* Property Detail */}
-      <section className="mb-12">
-        <PropertyDetail property={property} />
-      </section>
-
-      {/* Booking Section */}
-      <section className="mb-12">
-        <BookingSection price={property.price} />
-      </section>
-
-      {/* Review Section */}
-      <section className="mb-12">
-        <ReviewSection reviews={property.reviews} />
-      </section>
+      <PropertyDetail property={property} />
     </main>
   );
 };
